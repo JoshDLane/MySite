cfa8dd51f2d5e8aabcf543a7ad22926d98ff5637
Josh Lane <joshlane@Joshs-MBP.home>
2020-05-28 02:26:16 -0400

    small tweeks

--- src/screens/covidApp/covidApp.js
+++ src/screens/covidApp/covidApp.js
@@ -35,8 +35,8 @@ const CovidApp = (props) => {
                     </div>
                     <animated.div class="page_right" style={opacitySpring}>
                         <p className="myfont white">Integrated an API offered by the New York State Department of Health to provide 
-                            <span className="emphasized teal"> up to date, reliable, testing data on the county level</span> </p>
-                        <p className="myfont white">Utilized D3 to geomap the data and create an interactive line chart to view the data over time</p>
+                            <span className="emphasized teal"> up to date testing data on the county level</span> </p>
+                        <p className="myfont white">Utilized D3 to geomap the data and create an interactive time series chart</p>
                         <CheckoutGit link="https://github.com/JoshDLane/NYSCovidTesting"  color="white"/>
                     </animated.div>
                 </div>
