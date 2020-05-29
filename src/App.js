41695f85769bc2bd970b47a9ed17d5c5039d75c5
Josh Lane <joshlane@Joshs-MBP.home>
2020-05-29 12:42:58 -0400

    adding GA

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 import MainScroll from './components/mainScroll'
 import ContactModal from './components/contactModal/contactModal';
+import ReactGA from 'react-ga';
+
+
+function initializeReactGA() {
+  ReactGA.initialize('UA-168001915-1');
+  ReactGA.pageview('/homepage');
+}
 
 function App() {
+  initializeReactGA()
   return (
     <div className="App">
       <Switch>
--- src/screens/covidApp/covidApp.js
+++ src/screens/covidApp/covidApp.js
@@ -46,3 +46,4 @@ const CovidApp = (props) => {
 };  
 
 export default CovidApp;
+
