84e4532ae2a35abfb568504d648fb896cdf7be5b
Josh Lane <joshdlane22@gmail.com>
2020-06-04 13:01:06 -0400

    contact styling

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './contact.css'
+import '../../styles/rootstyles.css'
+
 const Contact = () => {
     return (
         <section id="contact" className="contact_sec_cont">
-            <div className="contact_text">
+            <div className="contact_text myfont">
                 <span className="contact_intro">Get in Touch!</span>
                 <span className="contact_intro detail">Always looking to discuss new projects</span>
                 <span className="contact_intro email">joshdlane22@gmail.com</span>
