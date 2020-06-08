4b3afb5cd65dfae7f453d5f8f50dec0bdcf3f276
Josh Lane <joshdlane22@gmail.com>
2020-06-08 12:13:41 -0400

    contact email linking:

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {Linking} from 'react';
 import './contact.css'
 import '../../styles/rootstyles.css'
 
 const Contact = () => {
+    function emailMe(){
+            window.location.href = 'mailto:joshdlane22@gmail.com';
+        }
     return (
         <section id="contact" className="contact_sec_cont">
             <div className="contact_text myfont">
                 <span className="contact_intro">Get in Touch!</span>
-                <span className="contact_intro detail">Always looking to discuss new projects</span>
-                <span className="contact_intro email">joshdlane22@gmail.com</span>
-
+                <p className="contact_intro detail">Always looking to discuss new projects</p>
+                <span className="contact_intro email" onClick={emailMe}>joshdlane22@gmail.com</span>
             </div>
         </section>
     );
