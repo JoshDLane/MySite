13ba8620095fc20c16e7334180d08bcba6b59e98
Josh Lane <joshdlane22@gmail.com>
2020-09-10 08:16:22 -0700

    deleting old poe policy page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import MainScroll from './components/mainScroll'
 import ReactGA from 'react-ga';
-import Poe from './pages/poe/poe'
 
 function initializeReactGA() {
   ReactGA.initialize('UA-168001915-1');
@@ -15,8 +14,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Switch>
-      <Route path="/poe" component={Poe} />
-        <Route path="/" component={MainScroll} />
+      <Route path="/" component={MainScroll} />
       </Switch>
       </BrowserRouter>
     </div>
